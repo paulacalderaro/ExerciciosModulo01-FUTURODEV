71ddbf9a02111a0d2da283e576de705861cd5530
paulacalderaro <paula.dcalderaro@gmail.com>
2025-03-04 09:43:50 -0300

    Add files via upload

--- whileTeste.java
+++ whileTeste.java
@@ -1,7 +1,11 @@
 package While;
 
+import java.util.Scanner;
+
 public class whileTeste {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] nomes = new String[5];
 
         int contador = 0;
 
@@ -13,9 +17,16 @@ public class whileTeste {
             if (contador == 4)
                 break;
 
-            {
+            int i;
+            for (i = 0; i < 3; i++) ;
+            System.out.println(++i);
+            System.out.println(i++);
 
-            }
+            double[] notas = {6.5, 7.8, 9, 3};
+            boolean[] valores_logicos = {true, false, false};
+
+
         }
     }
 }
+
